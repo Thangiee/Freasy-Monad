2c2781ef2b3106142120e074c5e4119cc840338f
Thangiee <Thangiee0@gmail.com>
2016-09-10 12:23:45 -0500

    Update intellij plugin for Free.inject, injectOps obj, and Inject class.

--- plugin/src/main/scala/com/thangiee/freasymonad/injector/freeInjector.scala
+++ plugin/src/main/scala/com/thangiee/freasymonad/injector/freeInjector.scala
@@ -1,11 +1,31 @@
 package com.thangiee.freasymonad.injector
 
+import com.intellij.psi.PsiClass
+import com.thangiee.freasymonad.injector.freeInjector._
 import org.jetbrains.plugins.scala.lang.psi.ScalaPsiUtil
 import org.jetbrains.plugins.scala.lang.psi.api.statements.{ScFunction, ScTypeAlias}
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef._
 import org.jetbrains.plugins.scala.lang.psi.impl.toplevel.typedef._
-import org.jetbrains.plugins.scala.lang.psi.types.api.Any
 import org.jetbrains.plugins.scala.lang.psi.types.{ScParameterizedType, ScType}
+
+object freeInjector {
+  implicit class RichFunc(val value: ScFunction) {
+    val typeParamsTxt: String = value.typeParametersClause.map(_.getText).getOrElse("")
+    val paramsTxt    : String = value.paramClauses.text
+    val argsText     : String = s"(${value.parameters.map(_.name).mkString(", ")})"
+
+    val returnTypeOrAny : ScType      = value.returnType.getOrAny
+    val returnInnerTypes: Seq[ScType] = returnTypeOrAny match {
+      case tpe: ScParameterizedType => tpe.typeArguments
+      case _ => Seq.empty
+    }
+    val returnInnerTypesTxt    : Seq[String] = returnInnerTypes.map(_.canonicalText)
+    val firstReturnInnerTypeTxt: String      = returnInnerTypesTxt.headOption.getOrElse("Any")
+
+    def appendTypeParam(tp: String): String =
+      if (typeParamsTxt.isEmpty) s"[$tp]" else s"[$tp, ${typeParamsTxt.tail.dropRight(1)}]"
+  }
+}
 
 class freeInjector extends SyntheticMembersInjector {
 
@@ -21,70 +41,74 @@ class freeInjector extends SyntheticMembersInjector {
             val typeAlias: Option[ScTypeAlias] = scTrait.aliases.headOption
             val typeAliasName: String = typeAlias.map(_.name).getOrElse("")
 
-            val sealedTrait = scTrait.getAllInnerClasses().collectFirst {
+            val absPath: String = s"${scTrait.getPath}.${scTrait.name}" // absolute path to trait companion obj
+
+            val sealedTrait: Option[PsiClass] = scTrait.getAllInnerClasses().collectFirst {
               case clazz if clazz.hasModifierProperty("sealed") => clazz
             }
             val sealedTraitName: String = sealedTrait.map(_.getName).getOrElse("")
+            val funcs: Seq[ScFunction] = scTrait.functions.filter(_.returnTypeOrAny.canonicalText.contains(typeAliasName))
+            val implicitInjectParam: String = s"(implicit I: cats.free.Inject[$absPath.$sealedTraitName, F])"
+            def Free(fn: ScFunction): String = s"cats.free.Free[F, ${fn.firstReturnInnerTypeTxt}]"
 
-            val (absFuncs, concreteFuncs): (Seq[ScFunction], Seq[ScFunction]) = scTrait.functions.partition(_.isAbstractMember)
-
-            val funcsReturnType: Seq[ScType] = absFuncs.map(_.returnType.getOrAny)
-            val funcsInnerReturnType: Seq[ScType] = funcsReturnType.collect {
-              case tpe: ScParameterizedType => tpe.typeArguments.headOption.getOrElse(Any)
-              case _ => Any
+            val sealedTraitADT = {
+              val caseClasses = funcs.map { fn =>
+                s"case class ${fn.name.capitalize}${fn.typeParamsTxt}${fn.paramsTxt} extends $sealedTraitName[${fn.firstReturnInnerTypeTxt}]"
+              }
+              s"object $sealedTraitName { ${caseClasses.mkString("\n")} }"
             }
-            val funcsName: Seq[String] = absFuncs.map(_.name)
-            val funcsTypeParamsTxt: Seq[String] = absFuncs.map(_.typeParametersClause.map(_.getText).getOrElse(""))
-            val funcsNameWithTp: Seq[String] = (funcsName zip funcsTypeParamsTxt).map{ case (name, tp) => name + tp }
-            val funcsParams: Seq[String] = absFuncs.map(_.paramClauses.text)
-            val funcsArgs: Seq[String] = absFuncs.map(_.parameters.map(_.name).mkString(", "))
-
-            val grammarADTClasses = (funcsNameWithTp zip funcsParams zip funcsInnerReturnType).map { case ((fName, params), rt) =>
-              s"case class ${fName.capitalize}$params extends $sealedTraitName[${rt.canonicalText}]"
-            }
-
-            val patternMatchCases = (funcsName zip funcsTypeParamsTxt zip funcsArgs).map { case ((fName, tp), args) =>
-              s"case ${fName.capitalize}$tp($args) => $fName($args)"
-            }
-
-            val funcsToBeImpl = (funcsNameWithTp zip funcsParams zip funcsInnerReturnType).map { case ((fName, params), rt) =>
-              s"def $fName$params: M[${rt.canonicalText}]"
-            }
-
-            val sealedTraitSig = sealedTrait.map(_.getText).getOrElse("")
-
-            val sealedTraitADT =
-              s"""
-                 |object $sealedTraitName {
-                 |  ${grammarADTClasses.mkString("\n")}
-                 |}
-               """.stripMargin
 
             val opsObj =
               s"""
                 |object ops {
                 |  ${typeAlias.map(_.text).getOrElse("")}
-                |  ${absFuncs.map(_.text).mkString("\n")}
-                |  ${concreteFuncs.map(_.text).mkString("\n")})
+                |  ${funcs.map(_.text).mkString("\n")}
                 |}
               """.stripMargin
 
-            val interpTrait =
+            val injectOpsObj = {
+              val ops = funcs.map { fn =>
+                s"def ${fn.name}${fn.appendTypeParam("F[_]")}${fn.paramsTxt}$implicitInjectParam: ${Free(fn)}) = ???"
+              }
+              s"object injectOps { ${ops.mkString("\n")} }"
+            }
+
+            val injectClass = {
+              val ops = funcs.map { fn =>
+                s"def ${fn.name}${fn.typeParamsTxt}${fn.paramsTxt}: ${Free(fn)} = ???"
+              }
+
+              s"class Inject[F[_]]$implicitInjectParam { ${ops.mkString("\n")} }"
+            }
+
+            val injectClassCompanion = {
+              s"""
+                 |object Inject {
+                 |  implicit def injectOps[F[_]]$implicitInjectParam: $absPath.Inject[F] = ???
+                 |}
+               """.stripMargin
+            }
+
+            val interpTrait = {
+              val absFuncs = funcs.filter(_.isAbstractMember)
+              val funcsToBeImpl = absFuncs.map { fn =>
+                s"def ${fn.name}${fn.typeParamsTxt}${fn.paramsTxt}: M[${fn.firstReturnInnerTypeTxt}]"
+              }
+
               s"""
                 |trait Interp[M[_]] {
-                |  import ${scTrait.getPath}.${scTrait.name}.ops._
                 |  import cats._
-                |  val interpreter = new ($sealedTraitName ~> M) {
-                |    def apply[A](fa: $sealedTraitName[A]): M[A] = fa match {
-                |      ${patternMatchCases.mkString("\n")}
-                |    }
+                |  val interpreter: $absPath.$sealedTraitName ~> M = {
+                |    def apply[A](fa: $absPath.$sealedTraitName[A]): M[A]
                 |  }
                 |  ${funcsToBeImpl.mkString("\n")}
-                |  def run[A](op: $typeAliasName[A])(implicit m: Monad[M], r: RecursiveTailRecM[M]): M[A] = op.foldMap(interpreter)
+                |  def run[A](op: $absPath.ops.$typeAliasName[A])(implicit m: Monad[M], r: RecursiveTailRecM[M]): M[A] = ???
                 |}
               """.stripMargin
+            }
 
-            Seq(sealedTraitSig, sealedTraitADT, opsObj, interpTrait)
+            val sealedTraitSig = sealedTrait.map(_.getText).getOrElse("")
+            Seq(sealedTraitSig, sealedTraitADT, opsObj, injectOpsObj, injectClass, injectClassCompanion, interpTrait)
 
           case _ => Seq.empty
         }
