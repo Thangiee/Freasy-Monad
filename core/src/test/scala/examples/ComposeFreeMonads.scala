e3cc43a67f04bb2bbec70be990fc1d0097e538b6
Thangiee <Thangiee0@gmail.com>
2016-09-10 16:44:35 -0500

    Add composing Free monads ADTs example.

---
+++ core/src/test/scala/examples/ComposeFreeMonads.scala
@@ -0,0 +1,74 @@
+package examples
+
+import cats.data.Coproduct
+import cats.free.Free
+import cats.{Id, ~>}
+import freasymonad.free
+
+import scala.collection.mutable.ListBuffer
+import scala.io.StdIn
+
+// example based off https://github.com/typelevel/cats/blob/master/docs/src/main/tut/freemonad.md#composing-free-monads-adts
+object ComposeFreeMonads extends App {
+
+  @free trait Interact {
+    type InteractF[A] = Free[Adt, A]
+    sealed trait Adt[A]
+    def ask(prompt: String): InteractF[String]
+    def tell(msg: String): InteractF[Unit]
+  }
+
+  @free trait DataSource {
+    type DataSourceF[A] = Free[Adt, A]
+    sealed trait Adt[A]
+    def addCat(a: String): DataSourceF[Unit]
+    def getAllCats: DataSourceF[List[String]]
+    def addAndGetAllCats(a: String): DataSourceF[List[String]] =
+     for {
+       _ <- addCat(a)
+       c <- getAllCats
+     } yield c
+  }
+
+  type CatsApp[A] = Coproduct[DataSource.Adt, Interact.Adt, A]
+
+  // program1 and program2 are the same.
+  // This library lets you choose which style you like.
+
+  def program1(implicit I: Interact.Inject[CatsApp], D : DataSource.Inject[CatsApp]): Free[CatsApp, Unit] = {
+    import I._, D._
+    for {
+      cat  <- ask("What's the kitty's name?")
+      cats <- addAndGetAllCats(cat)
+      _    <- tell(cats.toString)
+    } yield ()
+  }
+
+  val program2: Free[CatsApp, Unit] = {
+    import Interact.injectOps._, DataSource.injectOps._
+    for {
+      cat  <- ask[CatsApp]("What's the kitty's name?")
+      cats <- addAndGetAllCats[CatsApp](cat)
+      _    <- tell[CatsApp](cats.toString)
+    } yield ()
+  }
+
+  val consoleCats = new Interact.Interp[Id] {
+    def ask(prompt: String): Id[String] = {
+      println(prompt)
+      StdIn.readLine()
+    }
+    def tell(msg: String): Id[Unit] = println(msg)
+  }
+
+  val inMemoryDatasource = new DataSource.Interp[Id] {
+    private[this] val memDataSet = new ListBuffer[String]
+    def addCat(a: String): Id[Unit] = memDataSet.append(a)
+    def getAllCats: Id[List[String]] = memDataSet.toList
+  }
+
+  val interpreter: CatsApp ~> Id = inMemoryDatasource.interpreter or consoleCats.interpreter
+
+  program1.foldMap(interpreter)
+  program2.foldMap(interpreter)
+}
