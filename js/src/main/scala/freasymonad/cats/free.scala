c09db49cfba2392dbac773c432bea159df9051a6
Thang Le <tle@twitter.com>
2017-01-22 10:53:43 -0800

    add compileTimeOnly warning

--- js/src/main/scala/freasymonad/cats/free.scala
+++ js/src/main/scala/freasymonad/cats/free.scala
@@ -2,8 +2,10 @@ package freasymonad.cats
 
 import freasymonad.FreeImpl
 
+import scala.annotation.compileTimeOnly
 import scala.meta._
 
+@compileTimeOnly("enable macro paradise to expand macro annotations")
 class free extends scala.annotation.StaticAnnotation {
   inline def apply(defn: Any): Any = meta {
     FreeImpl.apply(defn, freasymonad.libs.Cats)
